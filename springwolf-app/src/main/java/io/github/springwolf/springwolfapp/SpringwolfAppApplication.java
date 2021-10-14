c8e99246280b29a74fcda6644a9d189e831b70bd
Stav Shamir <shamir.stav@gmail.com>
2021-10-14 09:15:28 +0300

    Initial commit

---
+++ springwolf-app/src/main/java/io/github/springwolf/springwolfapp/SpringwolfAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.springwolf.springwolfapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwolfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwolfAppApplication.class, args);
+	}
+
+}
---
+++ springwolf-app/src/test/java/io/github/springwolf/springwolfapp/SpringwolfAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.springwolf.springwolfapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwolfAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
